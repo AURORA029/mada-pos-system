a0e5724d39d698493034ebc834a1d1410cc8d9ab
ORGANES Isoambola <allan.rafidison@gmail.com>
2026-02-25 18:40:02 +0100

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, 'mada_pos.sqlite');
+const dbPath = path.join(process.cwd(), 'mada_pos.sqlite');
 
 const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
